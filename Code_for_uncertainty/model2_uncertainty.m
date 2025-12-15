08167edf44601587eadfb4a19100375c0d9182f5
Arka Roy <69377712+ArkaRoy-Matlab@users.noreply.github.com>
2025-12-15 23:55:12 +0530

    Update model2_uncertainty.m

--- Code_for_uncertainty/model2_uncertainty.m
+++ Code_for_uncertainty/model2_uncertainty.m
@@ -1,7 +1,7 @@
 % ***************************************************************
 % *** Matlab code for Uncertainty analysis of synthetic model (Reverse Fault)
 % *** Source Code is mainly written for research purposes. The codes are
-% *** having copyrights and required proper citations whenever it is used.
+% *** having copyrights and required proper citations whenbbever it is used.
 % *** Developed by:
 % ***       Mr. Arka Roy (email: arka.phy@gmail.com)
 % ***       Mr. Thatikonda Suresh Kumar (email: sureshkumarncess@gmail.com)
